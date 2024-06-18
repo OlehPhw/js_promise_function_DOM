eb42097687299945cf805cb3478c2b94ce44067f
Oleh K <olegk0409@gmail.com>
2024-06-18 13:31:31 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,31 @@
+"use strict";
+function waitFor(element, eventName) {
+    return new Promise((resolve)=>{
+        element.addEventListener(eventName, (e)=>{
+            const message = `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`;
+            resolve(message);
+        });
+    });
+}
+const printMessage = (message)=>{
+    const div = document.createElement("div");
+    div.classList.add("message");
+    div.textContent = message;
+    div.style.position = "fixed";
+    div.style.top = 0;
+    div.style.right = "10px";
+    document.body.append(div);
+};
+const loginField = document.getElementById("login");
+const passwordField = document.getElementById("password");
+const button = document.getElementById("submit");
+waitFor(loginField, "click").then(printMessage);
+waitFor(passwordField, "click").then(printMessage);
+waitFor(button, "click").then(printMessage);
+waitFor(loginField, "input").then(printMessage);
+waitFor(passwordField, "input").then(printMessage);
+waitFor(loginField, "blur").then(printMessage);
+waitFor(passwordField, "blur").then(printMessage);
+waitFor(button, "blur").then(printMessage);
+
+//# sourceMappingURL=index.f75de5e1.js.map
